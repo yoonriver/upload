8276958296a3ad76cfb1aa18b54ecbbeda4c76a7
YKS <styleyo12@naver.com>
2023-02-26 10:43:51 +0900

    Spring Upload-4

---
+++ upload/src/main/java/hello/upload/controller/ItemController.java
@@ -0,0 +1,30 @@
+package hello.upload.controller;
+
+import hello.upload.domain.ItemRepository;
+import hello.upload.file.FileStore;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemRepository itemRepository;
+    private final FileStore fileStore;
+
+    @GetMapping("/items/new")
+    public String newItem(@ModelAttribute ItemForm form) {
+        return "item-form";
+    }
+
+//    @PostMapping("/items/new")
+//    public String saveItem(@ModelAttribute ItemForm form, RedirectAttributes redirectAttributes) {
+//
+//    }
+}
